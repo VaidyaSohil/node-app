b84f2de91038b5698fb0cdae59999ce5e02a6309
sv <vaidyasohil@yahoo.com>
2021-04-25 17:11:43 -0600

    working chat-app with socketio

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+//io.on('connection', function (socket) {
+//  console.log('a user connected');
+//  socket.on('disconnect', () => {
+//    console.log('user disconnected');
+//  });
+//});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(5000, function () {
+  console.log('App listening on port 5000!');
 });
